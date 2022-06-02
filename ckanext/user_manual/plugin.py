a0ad32aff329e622205ba44775fb1cdcab65fb78
pooya.oa <pooya.oladazimi@tib.eu>
2022-06-02 11:13:55 +0200

    added help video

--- ckanext/user_manual/plugin.py
+++ ckanext/user_manual/plugin.py
@@ -1,10 +1,15 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 
 def help():
     return render_template('help.html')
+
+
+def get_help_video():
+    UPLOAD_DIR = toolkit.config['ckan.storage_path'] + '/storage/uploads/admin/'
+    return send_from_directory(UPLOAD_DIR, "help.mp4")
 
 
 class UserManualPlugin(plugins.SingletonPlugin):
@@ -28,6 +33,13 @@ class UserManualPlugin(plugins.SingletonPlugin):
             u'/user_manual/help',
             u'help',
             help,
+            methods=['GET']
+            )   
+        
+        blueprint.add_url_rule(
+            u'/user_manual/video',
+            u'video',
+            get_help_video,
             methods=['GET']
             )   
 
